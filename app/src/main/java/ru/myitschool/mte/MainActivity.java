898b9a1ba9f336afd3eec5bdbff3d0f0c1df5fb0
Yrka <114180251+Yrkaa@users.noreply.github.com>
2024-02-02 19:07:42 +0300

    Update MainActivity.java

--- app/src/main/java/ru/myitschool/mte/MainActivity.java
+++ app/src/main/java/ru/myitschool/mte/MainActivity.java
@@ -1,5 +1,7 @@
 package ru.myitschool.mte;
 
+import static java.lang.Thread.sleep;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnStart, btnStop;
 
+    private boolean runThread = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,58 +33,50 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                stopped = false;
-                fragmentThread.start();
+                if(!fragmentThread.isAlive())
+                    fragmentThread.start();
+                runThread = true;
+
             }
         });
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fragmentThread.interrupt();
-                stopped=true;
+                runThread = false;
             }
         });
 
 
     }
-    boolean stopped = true;
     Thread fragmentThread = new Thread(new Runnable() {
         @Override
         public void run() {
-            FirstFragment firstFragment = new FirstFragment();
-            ProceedingFragment proceedingFragment = new ProceedingFragment();
-
-            final boolean[] firstFragmentWatch = {true};
             final boolean[] firstItem = {true};
-
-            while (!stopped){
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
+            while(true){
+                if(runThread){
+                    //Основной код
+                    try {
+                        FirstFragment firstFragment = new FirstFragment();
+                        ProceedingFragment proceedingFragment = new ProceedingFragment();
                         FragmentTransaction fragmentTransaction = MainActivity.this.getSupportFragmentManager().beginTransaction();
-                        if(firstItem[0]){
-                            fragmentTransaction.add(R.id.output_fragment, firstFragment);
-                            firstItem[0] = false;
-                            firstFragmentWatch[0] = false;
-                        }
-                        else{
-                            if(firstFragmentWatch[0])
-                                fragmentTransaction.replace(R.id.output_fragment, proceedingFragment);
-                            else
-                                fragmentTransaction.replace(R.id.output_fragment, firstFragment);
-                            firstFragmentWatch[0] = !firstFragmentWatch[0];
-                        }
-                        fragmentTransaction.commit();
-
-                    }
-                });
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (firstItem[0])
+                                    fragmentTransaction.replace(R.id.output_fragment, firstFragment);
+                                else
+                                    fragmentTransaction.replace(R.id.output_fragment, proceedingFragment);
+                                firstItem[0] = !firstItem[0];
+                                fragmentTransaction.commit();
+                            }
+                        });
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {throw new RuntimeException(e);}
+                    //Основной код
                 }
             }
 
         }
+
     });
 }
